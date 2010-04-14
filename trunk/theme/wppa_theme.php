db00113071fc9c880a97c62cfaf672faa1fabdaa
opajaap <opajaap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-14 06:50:47 +0000

    Ver 1.8.3 released
    git-svn-id: https://plugins.svn.wordpress.org/wp-photo-album-plus@228762 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/theme/wppa_theme.php
+++ trunk/theme/wppa_theme.php
@@ -9,36 +9,59 @@ echo ($before_album);
 
 $mincount = get_option('wppa_min_thumbs', '1');
 
-if (wppa_page('albums')) { ?>
-
-    <div id="prevnext"><?php wppa_breadcrumb(); ?></div>
-<?php
+wppa_breadcrumb();
+echo('<br />&nbsp;');
+if (wppa_page('albums')) { 
     $albums = wppa_get_albums(); 
-    if ($albums) { ?>
+    if ($albums) { 
+?>
     <div id="albumlist">
 <?php
         $alt = 'even';
         foreach ($albums as $ta) :  global $album; $album = $ta;
             $photocount = wppa_get_photo_count();
             $albumcount = wppa_get_album_count();
+			if (is_numeric($album['cover_linkpage']) && $album['cover_linkpage'] > 0) {
+				$page_data = get_page($album['cover_linkpage']);
+				if (!empty($page_data) && $page_data->post_status == 'publish') {
+					$href = get_page_link($album['cover_linkpage']);
+					$title = __('Link to', 'wppa');
+					$title .= ' ' . $page_data->post_title;
+				} else {
+					$href = '#';
+					$title = __('Page is not available.');
+				}
+			} else {
+				if ($photocount != '0' && $photocount <= $mincount) {
+					$href = wppa_get_image_page_url(); 
+					$title = __('View the cover photo', 'wppa'); 
+						if ($photocount > 1) $title .= __('s', 'wppa');
+				} else {
+					$href = wppa_get_album_url(); 
+					$title = __('View the album', 'wppa') . ' ' . $album['name'];
+				}
+			}
+			$src = wppa_get_image_url();	
 ?>
  			<div class="album <?php echo($alt); ?>">
-				<a href="<?php if ($photocount <= $mincount) wppa_image_page_url(); else wppa_album_url(); ?>" title="<?php if ($photocount <= $mincount) { _e('View the cover photo', 'wppa'); if ($photocount > 1) _e('s', 'wppa'); } else { _e('View the album', 'wppa'); echo(' ' . $album['name']); } ?>">
-                    <img src="<?php wppa_image_url(); ?>" alt="<?php _e('View the album', 'wppa'); echo(' ' . $album['name']); ?>" class="image" />
-				</a>
+				<?php if ($src != '') { ?>
+					<a href="<?php echo($href); ?>" title="<?php echo($title); ?>">
+						<img src="<?php echo($src); ?>" alt="<?php echo($title); ?>" class="image" />
+					</a>
+				<?php } ?>
 				<h2 class="name">
-					<a href="<?php wppa_album_url(); ?>" title="<?php _e('View the album', 'wppa'); echo(' ' . $album['name']); ?>"><?php echo($album['name']); ?></a>
+					<a href="<?php echo($href); ?>" title="<?php echo($title); ?>"><?php echo($album['name']); ?></a>
 				</h2>
 				<p class="description"><?php wppa_the_album_desc(); ?></p>
 				<?php if ($photocount > $mincount) { ?>
 					<a href="<?php wppa_slideshow_url(); ?>" title="<?php _e('Slideshow', 'wppa'); ?>" ><?php _e('Slideshow', 'wppa'); ?></a>
 				<?php } ?>
 				<br/>
-				<?php if ($photocount > 1 || $albumcount) { ?>
-					<a href="<?php wppa_album_url(); ?>" title="<?php _e('View the album', 'wppa'); echo(' ' . $album['name']); ?>" ><?php _e('View', 'wppa'); ?>&nbsp;
-						<?php if ($albumcount) { echo($albumcount . ' '); _e('albums', 'wppa'); } ?>
+				<?php if ($photocount > $mincount || $albumcount) { ?>
+					<a href="<?php wppa_album_url(); ?>" title="<?php _e('View the album', 'wppa'); echo(' ' . $album['name']); ?>" ><?php _e('View', 'wppa'); ?>
+						<?php if ($albumcount) { echo(' ' . $albumcount . ' '); _e('albums', 'wppa'); } ?>
 						<?php if ($photocount > $mincount && $albumcount) _e('and', 'wppa'); ?>
-						<?php if ($photocount > $mincount) { echo($photocount . ' '); _e('photos', 'wppa'); } ?>
+						<?php if ($photocount > $mincount) { echo(' ' . $photocount . ' '); _e('photos', 'wppa'); } ?>
 					</a>
 				<?php } ?>
                 <div id="clear"></div>
@@ -62,8 +85,41 @@ if (wppa_page('albums')) { ?>
     <?php }
 }
 elseif (wppa_page('single')) { ?>
-    <div id="prevnext"><?php wppa_breadcrumb(); ?></div>
-	<?php wppa_prev_next('<div id="prev-arrow" class="prev"><a href="%link%">&laquo;</a></div>', '<div id="next-arrow" class="next"><a href="%link%">&raquo;</a></div>'); ?>
+	<script type="text/javascript">
+	/* <![CDATA[ */
+		var prev_href = '';
+		var next_href = '';
+		
+		function wppa_add_scroll(xid) {
+			var the_href;
+			var X = 0;
+			var Y = 0;
+			var id;
+			
+			if (xid == 0) id = 'p-a'; else id = 'n-a';
+			
+			if (id == 'p-a') {
+				if (prev_href == '') prev_href = document.getElementById('p-a').href;
+				the_href = prev_href;
+			}
+			else if (id == 'n-a') {
+				if (next_href == '') next_href = document.getElementById('n-a').href;
+				the_href = next_href;
+			}
+			else return; // Should never get here
+				
+			if (window.pageXOffset != 0) X = window.pageXOffset;
+			if (document.body.scrollLeft != 0) X = document.body.scrollLeft; /* IE */
+			if (window.pageYOffset != 0) Y = window.pageYOffset;
+			if (document.body.scrollTop != 0) Y = document.body.scrollTop; /* IE */
+			
+			document.getElementById(id).href = the_href + '&scrollx=' + X + '&scrolly=' + Y;
+		}
+	/* ]]> */
+	</script>
+<?php
+	wppa_prev_next('<div id="prev-arrow" class="prev"><a id="p-a" href="%link%" onmouseover="wppa_add_scroll(0)">&laquo;</a></div>', '<div id="next-arrow" class="next"><a id="n-a" href="%link%" onmouseover="wppa_add_scroll(1)">&raquo;</a></div>'); 
+?>
     <img src="<?php wppa_photo_url() ?>" alt="<?php wppa_photo_name() ?>" class="big" <?php wppa_fullsize(); ?> />
     <p id="imagedesc" class="imagedesc"><?php wppa_photo_desc(); ?></p>
 	<p id="imagetitle" class="imagetitle"><?php wppa_photo_name(); ?></p>
@@ -72,7 +128,6 @@ elseif (wppa_page('single')) { ?>
 elseif (wppa_page('slide')) {
 ?>
     <div id="prevnext">
-		<?php wppa_breadcrumb(); ?>
 		<p style="text-align: center">
 			<a href="#" id="speed0" onclick="wppa_speed(false)"><?php _e('Slower', 'wppa'); ?></a> |
 			<a href="#" id="startstop" onclick="wppa_startstop()"><?php _e('Start', 'wppa'); ?></a> |
