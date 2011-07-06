e9c13c57c3699285f85bd24966525e806babd7c8
opajaap <opajaap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-06 15:49:57 +0000

    Tag 3.0.7
    git-svn-id: https://plugins.svn.wordpress.org/wp-photo-album-plus@405529 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/3.0.7/wppa_admin.php
@@ -0,0 +1,152 @@
+<?php 
+/* wppa_admin.php
+* Package: wp-photo-album-plus
+*
+* Contains all the admin pages
+* Version 3.0.7
+*
+* dbg
+*/
+
+/* SETUP */
+//register_activation_hook(WPPA_FILE, 'wppa_setup');
+//act hook is useless since wp does no longer call this hook after upgrade of the plugin
+//this routine is now called at action admin_init, so also after initial install
+
+//Set force to true to re-run it even when on rev (happens in wppa_settings.php)
+
+function wppa_setup($force = false) {
+	global $wpdb;
+	global $wppa_revno;
+	
+	$old_rev = get_option('wppa_revision', '100');
+	if ($old_rev < $wppa_revno || $force) {
+		
+	$create_albums = "CREATE TABLE " . WPPA_ALBUMS . " (
+                    id bigint(20) NOT NULL auto_increment, 
+                    name text NOT NULL, 
+                    description text NOT NULL, 
+                    a_order smallint(5) unsigned NOT NULL, 
+                    main_photo bigint(20) NOT NULL, 
+                    a_parent bigint(20) NOT NULL,
+                    p_order_by int unsigned NOT NULL,
+					cover_linkpage bigint(20) NOT NULL,
+					owner text NOT NULL,
+                    PRIMARY KEY  (id) 
+                    );";
+                    
+	$create_photos = "CREATE TABLE " . WPPA_PHOTOS . " (
+                    id bigint(20) NOT NULL auto_increment, 
+                    album bigint(20) NOT NULL, 
+                    ext tinytext NOT NULL, 
+                    name text NOT NULL, 
+                    description longtext NOT NULL, 
+                    p_order smallint(5) unsigned NOT NULL,
+					mean_rating tinytext NOT NULL,
+					linkurl text NOT NULL,
+					linktitle text NOT NULL,
+                    PRIMARY KEY  (id) 
+                    );";
+
+	$create_rating = "CREATE TABLE " . WPPA_RATING . " (
+					id bigint(20) NOT NULL auto_increment,
+					photo bigint(20) NOT NULL,
+					value smallint(5) NOT NULL,
+					user text NOT NULL,
+					PRIMARY KEY  (id)
+					);";
+					
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+    dbDelta($create_albums);
+    dbDelta($create_photos);
+	dbDelta($create_rating);
+	
+	wppa_set_defaults();
+	wppa_check_dirs();
+	
+	$iret = true;
+	
+	if ($old_rev < '300') {		// theme and/or css changed since...
+		$key = '0';
+		$userstyle = ABSPATH.'wp-content/themes/'.get_option('template').'/wppa_style.css';
+		$usertheme = ABSPATH.'wp-content/themes/'.get_option('template').'/wppa_theme.php';
+		if (is_file($userstyle)) $key += '1';
+		if (is_file($usertheme)) $key += '2';
+		update_option('wppa_update_key', $key);
+		}
+	
+	if ($old_rev < '243') {		// ownerfield added in...
+		global $current_user;
+		get_currentuserinfo();
+		$user = $current_user->user_login;
+		$query = $wpdb->prepare('UPDATE `'.WPPA_ALBUMS.'` SET `owner` = %s WHERE `owner` = %s', $user, '');
+		$iret = $wpdb->query($query);
+		}
+		
+	if ($iret !== false) update_option('wppa_revision', $wppa_revno);
+//	echo('WPPA+ initialized');
+	}
+}
+
+/* FORM SECURITY */
+function wppa_nonce_field($action = -1, $name = 'wppa-update-check') { 
+	return wp_nonce_field($action, $name); 
+}
+function wppa_check_admin_referer($arg1, $arg2) {
+	check_admin_referer($arg1, $arg2);
+}
+
+/* ADMIN MENU */
+add_action('admin_menu', 'wppa_add_admin');
+
+function wppa_add_admin() {
+	global $wp_roles;
+
+	if (current_user_can('administrator')) {	// Make sure admin has access rights
+		$wp_roles->add_cap('administrator', 'wppa_admin');
+		$wp_roles->add_cap('administrator', 'wppa_upload');
+		$wp_roles->add_cap('administrator', 'wppa_sidebar_admin');
+	}
+
+	$iconurl = WPPA_URL.'/images/camera16.png';
+	add_menu_page('WP Photo Album', __('Photo&thinsp;Albums', 'wppa'), 'wppa_admin', WPPA_FILE, 'wppa_admin', $iconurl);
+	
+    add_submenu_page(WPPA_FILE, __('Upload Photos', 'wppa'), __('Upload Photos', 'wppa'), 'wppa_upload', 'upload_photos', 'wppa_page_upload');
+	add_submenu_page(WPPA_FILE, __('Import Photos', 'wppa'), __('Import Photos', 'wppa'), 'wppa_upload', 'import_photos', 'wppa_page_import');
+	add_submenu_page(WPPA_FILE, __('Export Photos', 'wppa'), __('Export Photos', 'wppa'), 'administrator', 'export_photos', 'wppa_page_export');
+    add_submenu_page(WPPA_FILE, __('Settings', 'wppa'), __('Settings', 'wppa'), 'administrator', 'options', 'wppa_page_options');
+	add_submenu_page(WPPA_FILE, __('Photo of the day Widget', 'wppa'), __('Photo of the day', 'wppa'), 'wppa_sidebar_admin', 'wppa_sidebar_options', 'wppa_sidebar_page_options');
+    add_submenu_page(WPPA_FILE, __('Help &amp; Info', 'wppa'), __('Help &amp; Info', 'wppa'), 'edit_posts', 'wppa_help', 'wppa_page_help');
+}
+
+/* ADMIN STYLES */
+add_action('admin_init', 'wppa_admin_styles');
+
+function wppa_admin_styles() {
+	wp_register_style('wppa_admin_style', WPPA_URL.'/admin_styles.css');
+	wp_enqueue_style('wppa_admin_style');
+}
+
+/* ADMIN SCRIPTS */
+add_action('admin_init', 'wppa_admin_scripts');
+
+function wppa_admin_scripts() {
+	wp_register_script('wppa_upload_script', WPPA_URL.'/multifile_compressed.js');
+	wp_enqueue_script('wppa_upload_script');
+	wp_register_script('wppa_admin_script', WPPA_URL.'/admin_scripts.js');
+	wp_enqueue_script('wppa_admin_script');
+	wp_enqueue_script('jquery');
+}
+
+/* ADMIN PHP's */
+require_once('wppa_albumadmin.php');
+require_once('wppa_upload.php');
+require_once('wppa_settings.php');
+require_once('wppa_widgetadmin.php');
+require_once('wppa_help.php');
+require_once('wppa_adminfunctions.php');
+require_once('wppa_export.php');
+
+// WP no longer runs the activation hook at update so we do it here
+add_action('admin_init', 'wppa_setup');
---
+++ tags/3.0.7/wppa_non_admin.php
@@ -0,0 +1,102 @@
+<?php 
+/* wppa_non_admin.php
+* Package: wp-photo-album-plus
+*
+* Contains all the non admin stuff
+* Version 3.0.7
+*
+*/
+
+/* API FILTER and FUNCTIONS */
+require_once('wppa_filter.php');
+require_once('wppa_functions.php');
+	
+/* LOAD STYLESHEET */
+add_action('wp_print_styles', 'wppa_add_style');
+
+function wppa_add_style() {
+	$userstyle = ABSPATH . 'wp-content/themes/' . get_option('template') . '/wppa_style.css';
+	if (is_file($userstyle)) {
+		wp_register_style('wppa_style', '/wp-content/themes/' . get_option('template')  . '/wppa_style.css');
+		wp_enqueue_style('wppa_style');
+	} else {
+		wp_register_style('wppa_style', WPPA_URL.'/theme/wppa_style.css');
+		wp_enqueue_style('wppa_style');
+	}
+}
+
+/* LOAD SLIDESHOW and THEME JS */
+add_action('init', 'wppa_add_javascripts');
+	
+function wppa_add_javascripts() {
+	wp_register_script('wppa_slideshow', WPPA_URL.'/theme/wppa_slideshow.js');
+	wp_register_script('wppa_theme_js', WPPA_URL.'/theme/wppa_theme.js');
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('wppa_slideshow');
+	wp_enqueue_script('wppa_theme_js');
+}
+	
+/* LOAD WPPA+ THEME */
+add_action('init', 'wppa_load_theme');
+	
+function wppa_load_theme() {
+	$templatefile = ABSPATH.'wp-content/themes/'.get_option('template').'/wppa_theme.php';
+	if (is_file($templatefile)) {
+		require_once($templatefile);
+	} else {
+		require_once('theme/wppa_theme.php');
+	}
+}
+	
+/* LOAD JS VARS AND ENABLE RENDERING */
+add_action('wp_head', 'wppa_kickoff', '100');
+
+function wppa_kickoff() {
+global $wppa;
+global $wppa_opt;
+
+	echo("\n<!-- WPPA+ Runtime parameters -->\n");
+	
+	/* This goes into wppa_theme.js */ 
+	echo('<script type="text/javascript">'."\n");
+	
+		echo("\t".'wppa_bgcolor_img = "'.$wppa_opt['wppa_bgcolor_img'].'";'."\n");
+		if ($wppa_opt['wppa_thumb_linktype'] == 'none') echo("\twppa_popup_nolink = true;\n"); 
+		else echo("\twppa_popup_nolink = false;\n");
+
+		/* This goes into wppa_slideshow.js */
+		if ($wppa_opt['wppa_fadein_after_fadeout']) echo("\t".'wppa_fadein_after_fadeout = true;'."\n");
+		else echo("\t".'wppa_fadein_after_fadeout = false;'."\n");
+		echo("\t".'wppa_animation_speed = '.$wppa_opt['wppa_animation_speed'].';'."\n");
+		echo("\t".'wppa_imgdir = "'.wppa_get_imgdir().'";'."\n");
+		if ($wppa['auto_colwidth']) echo("\t".'wppa_auto_colwidth = true;'."\n");
+		else echo("\t".'wppa_auto_colwidth = false;'."\n");
+		echo("\t".'wppa_thumbnail_area_delta = '.wppa_get_thumbnail_area_delta().';'."\n");
+		echo("\t".'wppa_textframe_delta = '.wppa_get_textframe_delta().';'."\n");
+		echo("\t".'wppa_box_delta = '.wppa_get_box_delta().';'."\n");
+		echo("\t".'wppa_ss_timeout = '.$wppa_opt['wppa_slideshow_timeout'].';'."\n");		
+		echo("\t".'wppa_preambule = '.wppa_get_preambule().';'."\n");
+		if ($wppa_opt['wppa_film_show_glue'] == 'yes') echo("\t".'wppa_film_show_glue = true;'."\n");
+		else echo("\t".'wppa_film_show_glue = false;'."\n");
+		echo("\t".'wppa_slideshow = "'.__a('Slideshow', 'wppa_theme').'";'."\n");
+		echo("\t".'wppa_start = "'.__a('Start', 'wppa_theme').'";'."\n");
+		echo("\t".'wppa_stop = "'.__a('Stop', 'wppa_theme').'";'."\n");
+		echo("\t".'wppa_photo = "'.__a('Photo', 'wppa_theme').'";'."\n");
+		echo("\t".'wppa_of = "'.__a('of', 'wppa_theme').'";'."\n");
+		echo("\t".'wppa_prevphoto = "'.__a('Prev.&nbsp;photo', 'wppa_theme').'";'."\n");
+		echo("\t".'wppa_nextphoto = "'.__a('Next&nbsp;photo', 'wppa_theme').'";'."\n");
+		echo("\t".'wppa_prevp = "'.__a('Prev.', 'wppa_theme').'";'."\n");
+		echo("\t".'wppa_nextp = "'.__a('Next', 'wppa_theme').'";'."\n");
+		echo("\t".'wppa_username = "'.wppa_get_user().'";'."\n");
+		if ($wppa_opt['wppa_rating_change'] || $wppa_opt['wppa_rating_multi']) echo("\t".'wppa_rating_once = false;'."\n");
+		else echo("\t".'wppa_rating_once = true;'."\n");
+
+	echo("</script>\n");
+	
+	$wppa['rendering_enabled'] = true;
+	echo("\n<!-- WPPA+ Rendering enabled -->\n");
+	if ($wppa['debug']) {
+		error_reporting($wppa['debug']);
+		add_action('wp_footer', 'wppa_phpinfo');
+	}
+}
