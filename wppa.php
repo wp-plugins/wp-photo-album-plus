10ed49c430a8c19f4362f587c00c805dd3fb158b
opajaap <opajaap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-26 14:27:29 +0000

    Release 4.0.10 language update and version number
    git-svn-id: https://plugins.svn.wordpress.org/wp-photo-album-plus/trunk@429055 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wppa.php
+++ wppa.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Photo Album Plus
 Description: Easily manage and display your photo albums and slideshows within your WordPress site.
-Version: 4.0.9
+Version: 4.0.10
 Author: J.N. Breetvelt a.k.a OpaJaap
 Author URI: http://www.opajaap.nl/
 Plugin URI: http://wordpress.org/extend/plugins/wp-photo-album-plus/
