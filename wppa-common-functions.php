8dcd65851851fa813878897123132af3ba9e8c59
opajaap <opajaap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-08 15:39:47 +0000

    Release.4.4.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-photo-album-plus/trunk@516314 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wppa-common-functions.php
+++ wppa-common-functions.php
@@ -2,11 +2,11 @@
 /* wppa-common-functions.php
 *
 * Functions used in admin and in themes
-* version 4.4.0
+* version 4.4.1
 *
 */
 global $wppa_api_version;
-$wppa_api_version = '4-4-0-000';
+$wppa_api_version = '4-4-1-000';
 // Initialize globals and option settings
 function wppa_initialize_runtime($force = false) {
 global $wppa;
