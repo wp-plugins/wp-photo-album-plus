02303529027ae32ad8366af1536221de5decca21
opajaap <opajaap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-15 10:11:02 +0000

    Release 5.1.6
    git-svn-id: https://plugins.svn.wordpress.org/wp-photo-album-plus@772613 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/wppa.php
+++ trunk/wppa.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Photo Album Plus
 Description: Easily manage and display your photo albums and slideshows within your WordPress site.
-Version: 5.1.5
+Version: 5.1.6
 Author: J.N. Breetvelt a.k.a OpaJaap
 Author URI: http://wppa.opajaap.nl/
 Plugin URI: http://wordpress.org/extend/plugins/wp-photo-album-plus/
@@ -17,11 +17,11 @@ global $wpdb;
 /* when new options are added and when the wppa_setup() routine 
 /* must be called right after update for any other reason.
 */
-global $wppa_revno; 		$wppa_revno = '5105';	
+global $wppa_revno; 		$wppa_revno = '5106';	
 /* This is the api interface version number
 /* It is incremented at any code change.
 */
-global $wppa_api_version; 	$wppa_api_version = '5-1-5-000';
+global $wppa_api_version; 	$wppa_api_version = '5-1-6-000';
 
 /* CONSTANTS
 /*
