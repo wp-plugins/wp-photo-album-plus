fc572632e2c5140b559fec380f247246cbc35366
opajaap <opajaap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-27 12:56:14 +0000

    Upd 5.1.4.rc3 to 5.1.8.rc1 version no
    git-svn-id: https://plugins.svn.wordpress.org/wp-photo-album-plus@779212 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/5.1.8.rc1/wppa.php
+++ tags/5.1.8.rc1/wppa.php
@@ -21,7 +21,7 @@ global $wppa_revno; 		$wppa_revno = '5108';
 /* This is the api interface version number
 /* It is incremented at any code change.
 */
-global $wppa_api_version; 	$wppa_api_version = '5-1-8-000';
+global $wppa_api_version; 	$wppa_api_version = '5-1-8-001';
 
 /* CONSTANTS
 /*
