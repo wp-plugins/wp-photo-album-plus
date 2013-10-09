2ba127c249ac0406593316de48110e82acca1d5f
opajaap <opajaap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-09 12:18:56 +0000

    Patch for single vote thumb
    git-svn-id: https://plugins.svn.wordpress.org/wp-photo-album-plus/trunk@785221 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wppa.php
+++ wppa.php
@@ -21,7 +21,7 @@ global $wppa_revno; 		$wppa_revno = '5109';
 /* This is the api interface version number
 /* It is incremented at any code change.
 */
-global $wppa_api_version; 	$wppa_api_version = '5-1-9-000';
+global $wppa_api_version; 	$wppa_api_version = '5-1-9-001';
 
 /* CONSTANTS
 /*
