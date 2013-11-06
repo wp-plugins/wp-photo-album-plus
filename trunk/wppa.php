032f2b57545eae3018b137b369920884f6525153
opajaap <opajaap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-06 16:16:18 +0000

    Release 5.1.16.001
    git-svn-id: https://plugins.svn.wordpress.org/wp-photo-album-plus@799890 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/wppa.php
+++ trunk/wppa.php
@@ -21,7 +21,7 @@ global $wppa_revno; 		$wppa_revno = '5116';
 /* This is the api interface version number
 /* It is incremented at any code change.
 */
-global $wppa_api_version; 	$wppa_api_version = '5-1-16-000';
+global $wppa_api_version; 	$wppa_api_version = '5-1-16-001';
 
 /* CONSTANTS
 /*
